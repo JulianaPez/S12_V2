6660dc9ec0610656fca78829716634ba11c01d56
KarinaSabogal <88290156+KarinaSabogal@users.noreply.github.com>
2021-10-09 23:33:48 -0500

    Create Clases.py

---
+++ Clases.py
@@ -0,0 +1,30 @@
+class Usurio:
+
+    def __init__(self, name, password, correo, profesion, telefono, ubicacion, rol):
+        self.name = name
+        self.password = password
+        self.correo = correo
+        self.profesion = profesion
+        self.telefono = telefono
+        self.ubicacion = ubicacion
+        self.rol =rol
+
+class biologo(Usuario):
+   def __init__(self, credencial)
+   self.credencial =credencial 
+  
+class administrador(Usuario):
+   def __init__(self, permisos)
+   self.permisos = permisos
+
+  
+class individuo:
+
+    def __init__(self, id, especie, orden, genero, familia, ubicacion, rol):
+        self.name = name
+        self.password = password
+        self.correo = correo
+        self.profesion = profesion
+        self.telefono = telefono
+        self.ubicacion = ubicacion
+        self.rol =rol  
