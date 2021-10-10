e72ffa58d8d129282810b3377bc6f3959edf32a7
KarinaSabogal <88290156+KarinaSabogal@users.noreply.github.com>
2021-10-09 23:18:47 -0500

    Update conexionDB.py

--- conexionDB.py
+++ conexionDB.py
@@ -1,10 +1,11 @@
+###CONEXION BASE DE DATOS###
 import pymysql
 
 HOST = "localhost"
 PORT = 3306
 USER = "root"
 PSS = "admin"
-DB = "misiontic"
+DB = "colecciones"
 
 def connetDB():
 
@@ -15,5 +16,30 @@ def connetDB():
     except:
         print("No se pudo conectar")
         return conn
-
     
+###INSERTAR FILAS###
+cursor = db.cursor()
+sql = "INSERT INTO individuo(id, clase, genero, familia, especie, ubicación, imagen ) \
+   VALUES (NULL,'{0}','{1}','{2}','{3}','{4}','{5}')".format("clase", "genero", "familia", "especie", "ubicación", "imagen")  #valores cambiaran en cada fila#
+try:
+   cursor.execute(sql)
+   db.commit()
+except:
+   db.rollback()
+
+###LEER BASE DE DATOS###
+cursor = db.cursor()
+
+sql = "SELECT * FROM individuo \
+WHERE id > {0}".format(0)
+cursor.execute(sql)
+results = cursor.fetchall()
+for row in results:
+   id = row[0]
+   clase= row[1]
+   genero= row[2]
+   familia= row[3]
+   especie= row[4]
+   ubicacion= row[5]
+   imagen= row[6]
+   print ("id = {0}, clase= {1}, genero= {2}, familia= {3}, especie= {4}, ubicación= {5}, imagen= {6}".format(id, clase, genero, familia, especie, ubicación, imagen))
